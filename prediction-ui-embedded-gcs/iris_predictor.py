382add30619a655cb2471fbdce018d9ce7e6e486
brand <p.h.grillobrandao@tilburguniversity.edu>
2022-10-24 16:33:51 +0200

    fix pickled

--- prediction-ui-embedded-gcs/iris_predictor.py
+++ prediction-ui-embedded-gcs/iris_predictor.py
@@ -2,6 +2,7 @@ import json
 
 import pandas as pd
 from keras.models import load_model
+import pickle
 
 #import pickle #extra add to read the model
 
@@ -11,8 +12,8 @@ class IrisPredictor:
 
     def predict_single_record(self, df):
         if self.model is None:
-            self.model = load_model('model.h5')
-
+            with open('model.h5', 'rb') as open_file:
+                self.model = pickle.load(open_file)
         y_pred = self.model.predict(df)
         print(y_pred[0])
         status = (y_pred[0])
