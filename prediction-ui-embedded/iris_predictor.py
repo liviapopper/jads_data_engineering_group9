a930391931614371489033aaaf96aa29dcf072dc
brand <p.h.grillobrandao@tilburguniversity.edu>
2022-10-24 16:29:18 +0200

    fix pickled

--- prediction-ui-embedded/iris_predictor.py
+++ prediction-ui-embedded/iris_predictor.py
@@ -13,7 +13,7 @@ class IrisPredictor:
     def predict_single_record(self, df):
         if self.model is None:
             with open('model.h5', 'rb') as open_file:
-                model = pickle.load(open_file)
+                self.model = pickle.load(open_file)
 
         y_pred = self.model.predict(df)
         print(y_pred[0])
