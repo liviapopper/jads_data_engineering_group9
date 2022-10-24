dfa8f73e1413357a1b30496a641c21dbde8be90e
brand <p.h.grillobrandao@tilburguniversity.edu>
2022-10-24 16:50:30 +0200

    fix pickled

--- prediction-ui-embedded-gcs/iris_predictor.py
+++ prediction-ui-embedded-gcs/iris_predictor.py
@@ -2,19 +2,20 @@ import json
 
 import pandas as pd
 from keras.models import load_model
+import os
 import pickle
 
-#import pickle #extra add to read the model
 
 class IrisPredictor:
     def __init__(self):
         self.model = None
 
     def predict_single_record(self, df):
+        model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
         if self.model is None:
             with open('model.h5', 'rb') as open_file:
                 self.model = pickle.load(open_file)
-                
+
         y_pred = self.model.predict(df)
         print(y_pred[0])
         status = (y_pred[0])
