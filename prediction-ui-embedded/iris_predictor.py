102f60f3a81a1587fefc4b8b1cd053dcba49f721
brand <p.h.grillobrandao@tilburguniversity.edu>
2022-10-24 10:31:27 +0200

    printing species type

--- prediction-ui-embedded/iris_predictor.py
+++ prediction-ui-embedded/iris_predictor.py
@@ -8,10 +8,12 @@ class IrisPredictor:
     def __init__(self):
         self.model = None
 
+    species = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+
     def predict_single_record(self, df):
         if self.model is None:
             self.model = load_model('model.h5')
         y_pred = self.model.predict(df)
-        print(y_pred[0])
-        status = (y_pred[0])
+        print(species[np.argmax(y_pred)])
+        status = species[np.argmax(y_pred)]
         return status
